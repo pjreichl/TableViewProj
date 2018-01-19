688be519dd10ae649b73b6bf63b5f9bbc9bbea6f
Peter Reichl <pjreichl@gmail.com>
2018-01-19 16:51:57 -0500

    Initial Commit

---
+++ TableViewProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewProj
+//
+//  Created by Peter Reichl on 1/19/18.
+//  Copyright © 2018 Peter Reichl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
