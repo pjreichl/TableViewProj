a64fada740cf4e20b44d18b18ceaea5d48ff5426
Peter Reichl <pjreichl@gmail.com>
2018-01-22 16:04:56 -0500

    First full attempt

---
+++ GuitarPractice.swift
@@ -0,0 +1,21 @@
+//
+//  DataItemFile.swift
+//  TableViewProj
+//
+//  Created by Peter Reichl on 1/22/18.
+//  Copyright © 2018 Peter Reichl. All rights reserved.
+//
+
+import UIKit
+
+class GuitarPractice {
+    var skill: String
+    var reps: Int
+    var exerciseType: String
+    
+    init(skill: String, reps: Int, exerciseType: String) {
+        self.skill = skill
+        self.reps = reps
+        self.exerciseType = exerciseType
+  }
+}
--- TableViewProj/TableViewController.swift
+++ TableViewProj/TableViewController.swift
@@ -11,8 +11,143 @@ import UIKit
 class TableViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var items = [GuitarPractice]()
+    var otherItems = [GuitarPractice]()
+    var allItems = [[GuitarPractice]]()
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return allItems.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        let addedRow = isEditing ? 1 :0
+        return allItems[section].count + addedRow
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        if indexPath.row >= allItems[indexPath.section].count, isEditing{
+            cell.textLabel?.text = "Add Random Exercise"
+            cell.detailTextLabel?.text = nil
+        } else {
+            let item = allItems[indexPath.section][indexPath.row]
+            
+            cell.textLabel?.text = item.skill
+            cell.detailTextLabel?.text = String(item.reps)
+            
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        //        if editingStyle == .delete {
+        //            allItems[indexPath.section].remove(at: indexPath.row)
+        //            tableView.deleteRows(at: [indexPath], with: .fade)
+        //        }
+        
+        if editingStyle == .delete {
+            allItems[indexPath.section].remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            let newData = GuitarPractice(skill: "New Data", reps: 0, exerciseType: "")
+            allItems[indexPath.section].append(newData)
+            tableView.insertRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        let sectionItems = allItems[indexPath.section]
+        if indexPath.row >= sectionItems.count {
+            return .insert
+        } else {
+            return .delete
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        let sectionItems = allItems[indexPath.section]
+        if isEditing && indexPath.row < sectionItems.count {
+            return nil
+        }
+        return indexPath
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let sectionItems = allItems[indexPath.section]
+        if indexPath.row >= sectionItems.count && isEditing {
+            self.tableView(tableView, commit: .insert, forRowAt: indexPath)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        let sectionItems = allItems[indexPath.section]
+        if indexPath.row >= sectionItems.count && isEditing {
+            return false
+        }
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let itemToMove = allItems[sourceIndexPath.section][sourceIndexPath.row]
+        
+        allItems[sourceIndexPath.section].remove(at: sourceIndexPath.row)
+        
+        if sourceIndexPath.section == destinationIndexPath.section {
+            allItems[sourceIndexPath.section].insert(itemToMove, at: destinationIndexPath.row)
+        } else {
+            allItems[destinationIndexPath.section].insert(itemToMove, at: destinationIndexPath.row)
+        }
+    }
+    
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        
+        if editing {
+            tableView.beginUpdates()
+            
+            for (index, sectionItems) in allItems.enumerated() {
+                let indexPath = IndexPath(row: sectionItems.count, section: index)
+                tableView.insertRows(at: [indexPath], with: .fade)
+            }
+            
+            tableView.endUpdates()
+            tableView.setEditing(true, animated: true)
+        } else {
+            tableView.beginUpdates()
+            
+            for (index, sectionItems) in allItems.enumerated() {
+                let indexPath = IndexPath(row: sectionItems.count, section: index)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            
+            tableView.endUpdates()
+            tableView.setEditing(false, animated: true)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = editButtonItem
+        tableView.allowsSelectionDuringEditing = true
+        
+        for i in 1...12 {
+            if i > 9 {
+                items.append(GuitarPractice(skill: "New Data", reps: 0, exerciseType: ""))
+            } else {
+                items.append(GuitarPractice(skill: "New Data", reps: 0, exerciseType: ""))
+            }
+        }
+        allItems.append(items)
+        allItems.append(otherItems)
+        
+        tableView.allowsSelectionDuringEditing = true
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,14 +156,6 @@ class TableViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
-}
-
-extension TableViewController: UITableViewDataSource {
-}
-
-
-extension TableViewController: UITableViewDelegate {
 }
 
 
